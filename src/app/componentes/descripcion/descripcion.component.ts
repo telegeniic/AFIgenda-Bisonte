525007918863f64718cd868c8c4c1558f13d0a98
Martin Guajardo <martinguajardo20@gmail.com>
2020-12-04 17:12:35 -0600

    Ahora la fecha ya se muestra correctamente

--- src/app/componentes/descripcion/descripcion.component.ts
+++ src/app/componentes/descripcion/descripcion.component.ts
@@ -17,13 +17,30 @@ interface event{
 })
 export class DescripcionComponent implements OnInit {
 
+  diaI:Date
+  horaI:Date
+  diaITxt:String
+  horaITxt:String
+  diaF:Date
+  horaF:Date
+  diaFTxt:String
+  horaFTxt:String
+
   @Input() e:event 
 
   constructor() { }
 
   ngOnInit() {
-    console.log("ng on init descripcion")
-    console.log(this.e)
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    this.diaI = new Date(this.e.eventInicioDia)
+    this.diaITxt = `${this.diaI.toLocaleDateString('es-MX',options)}`
+    this.horaI = new Date(this.e.eventInicioHora);
+    this.horaITxt = `${this.horaI.toLocaleTimeString('es-MX',{hour: '2-digit', minute:'2-digit'})}`;
+    this.diaF = new Date(this.e.eventTerminaDia)
+    this.diaFTxt = `${this.diaF.toLocaleDateString('es-MX',options)}`
+    console.log(this.diaITxt);
+    this.horaF = new Date(this.e.eventTerminaHora);
+    this.horaFTxt = `${this.horaF.toLocaleTimeString('es-MX',{hour: '2-digit', minute:'2-digit'})}`;
   }
 
 }
--- src/app/componentes/eventos/eventos.component.ts
+++ src/app/componentes/eventos/eventos.component.ts
@@ -17,23 +17,39 @@ interface event{
 })
 export class EventosComponent implements OnInit {
 
-  data = {
-    tittle: "Evento prueba",
-    description: "Descripcion de prueba",
-    startingDate: "Inicio Prueba",
-    finishDate: "Final Prueba",
-    location: "Localizacion Prueba"
-  }
-
+  diaI:Date
+  horaI:Date
+  diaITxt:String
+  horaITxt:String
+  diaF:Date
+  horaF:Date
+  diaFTxt:String
+  horaFTxt:String
   viewDescripcion: boolean = false;
   @Input() e:event;
   @Output() messageEvent = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {
+    
+   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    this.diaI = new Date(this.e.eventInicioDia)
+    this.diaITxt = `${this.diaI.toLocaleDateString('es-MX',options)}`
+    this.horaI = new Date(this.e.eventInicioHora);
+    this.horaITxt = `${this.horaI.toLocaleTimeString('es-MX',{hour: '2-digit', minute:'2-digit'})}`;
+    this.diaF = new Date(this.e.eventTerminaDia)
+    this.diaFTxt = `${this.diaF.toLocaleDateString('es-MX',options)}`
+    console.log(this.diaITxt);
+    this.horaF = new Date(this.e.eventTerminaHora);
+    this.horaFTxt = `${this.horaF.toLocaleTimeString('es-MX',{hour: '2-digit', minute:'2-digit'})}`;
+  }
 
   openDescription(){
+    this.diaI = new Date(this.e.eventInicioDia)
+    console.log(this.diaI.getMonth());
+    console.log(this.diaF);
     this.viewDescripcion = !this.viewDescripcion
     if(this.viewDescripcion){
       this.messageEvent.emit(this.e)
